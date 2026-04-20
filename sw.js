4b46bbb709a815fd37ced8deeca44f79c5704b5a
zafar358 <nadie442@icloud.com>
2026-04-20 19:35:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'lista-nave-v1';
+const ASSETS = [
+  'lista_compra.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap',
+  'https://unpkg.com/lucide@latest',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
